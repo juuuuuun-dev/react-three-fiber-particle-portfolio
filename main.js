3863cc22b639ff23d75cf06ea5091807565e7c30
junkata <junkata.dev@gmail.com>
2019-11-12 12:43:44 +0900

    temp commit

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
+// @todo scene add loop
 
-var imgSrc = "images/a.png"
+var imgSrc = "images/a.png";
+var imgSrcArr = [
+  {
+    src: 'images/a.png',
+  },
+  {
+    src: 'images/cover.png',
+  },
+]
+var imageIndex = 0;
 var image, objLoader, bunnyGeo, particle, clock;
 var camera, scene, renderer;
 var texture;
 var datas = [];
-var targetArr = [];
+var imagePositions = [];
 var MAX = 50000;
 
-imgIni();
+imageInit();
+imagesInit();
 
 function range(min, max){
     return min + (max - min) * Math.random();
@@ -22,7 +33,7 @@ class CustomParticle extends THREE.BufferGeometry {
         var alphaArray = new Float32Array(this.count);
         var timeArray = new Float32Array(this.count);
         var colorArray = new Float32Array(this.count * 3);
-        
+        const datas = imagePositions[imageIndex];
         for(var ii = 0; ii < this.count; ii++){
             var data = datas[parseInt(datas.length * Math.random())];
             
@@ -69,15 +80,80 @@ class CustomMat extends THREE.ShaderMaterial {
     }
 }
 
-function imgIni(){
+function imageInit(){
     image = new Image();
     image.onload = onLoadimage;
     image.crossOrigin = "Anonymous";
     image.src = imgSrc;
 }
 
+/**
+ * imagesInit
+ * array image ver
+ */
+async function imagesInit() {
+  var canvas = document.createElement("canvas");
+  imagePositions = await createImgPosition(canvas);
+  console.log(imagePositions);
+  init();
+}
 
-function init(){
+/**
+ * createImgPosition
+ * @param {*} canvas
+ */
+function createImgPosition(canvas) {
+  return new Promise(async (resolve) => {
+    let positions = [];
+    var len = imgSrcArr.length;
+    var scale = 10;
+    for (var i = 0; len > i; i++) {
+      let pos = [];
+      await loadImage(imgSrcArr[i].src)
+        .then(image => {
+          canvas.width = image.width;
+          canvas.height = image.height;
+          var ctx = canvas.getContext('2d');
+          ctx.drawImage(image, 0, 0);
+          var data = ctx.getImageData(0, 0, image.width, image.height);
+          var imageData = data.data;
+          for (var y = 0; y < image.height; y++) {
+            for (var x = 0; x < image.width; x++) {
+              var num = 4 * (image.width * y + x) + 3;
+              var alpha = imageData[num];
+              if (alpha !== 0) {
+                var rRate = x / image.width * .9;
+                var gRate = y / image.height * .2;
+                var color = new THREE.Color();
+                color.setRGB(rRate, gRate, 1);
+                var data = {
+                  x: (x - image.width / 2 + 9 * Math.random()) / scale,
+                  y: (y - image.height / 2) / scale,
+                  alpha: alpha / 255,
+                  color,
+                }
+                pos.push(data)
+              }
+            }
+          }
+          positions.push(pos);
+        })
+    }
+    resolve(positions);
+  })
+}
+
+function loadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.crossOrigin = "Anonymous";
+    img.onload = () => resolve(img);
+    img.onerror = e => reject();
+    img.src = src;
+  });
+}
+
+function  init(){
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
     camera.position.z = -30;
     camera.position.y = 100;
@@ -97,6 +173,7 @@ function init(){
 
     document.addEventListener('mousemove', onDocumentMouseMove, false);
 
+    // requestAnimationFrame
     TweenMax.ticker.addEventListener("tick", loop);
 }
 
@@ -123,15 +200,18 @@ function onLoadimage(){
                 color.setRGB(rRate, gRate, 1);
                 // color.setRGB(colorRate, 0.1 * Math.random(), 1)
                 // color.setHSL(colorRate, 0.3 + 0.6 * Math.random(), 0.3 + 0.3 * Math.random())
-                var data = {x: (xx - image.width / 2 + 9 * Math.random()) / scale, y: (yy - image.height / 2) / scale, alpha: alpha / 255, color: color }
+                var data = {
+                  x: (xx - image.width / 2 + 9 * Math.random()) / scale,
+                  y: (yy - image.height / 2) / scale,
+                  alpha: alpha / 255,
+                  color: color,
+                }
                 datas.push(data);
             }
 
         }
     }
-
-
-    init();
+    // init();
 }
 
 var theta = 0;
@@ -141,14 +221,11 @@ var mouse = new THREE.Vector2();
 function loop(){
     var delta = clock.getDelta();
     time += delta;
-
     theta += (mouse.x /3 - theta)/10;
 
     camera.position.z = 10 * Math.cos(theta);
     camera.position.x = 10 * Math.sin(theta);
     camera.lookAt(new THREE.Vector3())
-
-
 
     particle.material.uniforms.uTime.value = time;
     renderer.render(scene, camera);
