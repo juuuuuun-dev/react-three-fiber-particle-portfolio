de48d18ac12bdd59a70b8eee16a736590df13a18
kata <kumaoooooo@yahoo.co.jp>
2019-11-13 12:19:23 +0900

    create function createAttributes

--- main.js
+++ main.js
@@ -16,94 +16,136 @@ var texture;
 var datas = [];
 var imagePositions = [];
 var MAX = 50000;
+let attributes = {
+  positions: [],
+  endPositions: [],
+  alphas: [],
+  colors: [],
+};
 
-imageInit();
 imagesInit();
 
-function range(min, max){
-    return min + (max - min) * Math.random();
+
+/**
+ * 画像初期設定
+ */
+async function imagesInit() {
+  const canvas = document.createElement("canvas");
+  imagePositions = await createImagePositions(canvas);
+  attributes = await createAttributes(imagePositions);
+  init();
 }
 
-class CustomParticle extends THREE.BufferGeometry {
-    constructor(){
-        super()
-        this.count = MAX;
-        var positionArray = new Float32Array(this.count * 3);
-        var endPositionArray = new Float32Array(this.count * 3);
-        var alphaArray = new Float32Array(this.count);
-        var timeArray = new Float32Array(this.count);
-        var colorArray = new Float32Array(this.count * 3);
-        const datas = imagePositions[imageIndex];
-        // @todo これを別のfunctionにと画像配列分
-        for(var ii = 0; ii < this.count; ii++){
-            var data = datas[parseInt(datas.length * Math.random())];
-            
-            positionArray[ii * 3 + 0] = data.x;
-            positionArray[ii * 3 + 1] = 0;
-            positionArray[ii * 3 + 2] = data.y;
-
-            endPositionArray[ii * 3 + 0] = range(0, 30);
-            endPositionArray[ii * 3 + 1] = range(0, 30);
-            endPositionArray[ii * 3 + 2] = range(0, 0);
-
-            timeArray[ii] = 3 * Math.random();
-
-            alphaArray[ii] = Math.random();
-
-            colorArray[ii * 3 + 0] = data.color.r;
-            colorArray[ii * 3 + 1] = data.color.g;
-            colorArray[ii * 3 + 2] = data.color.b;
-
-        }
-
-        this.addAttribute("position", new THREE.BufferAttribute(positionArray, 3));
-        this.addAttribute("aTarget", new THREE.BufferAttribute(endPositionArray, 3));
-        this.addAttribute("aTime", new THREE.BufferAttribute(timeArray, 1));
-        this.addAttribute("aAlpha", new THREE.BufferAttribute(alphaArray, 1));
-        this.addAttribute("aColor", new THREE.BufferAttribute(colorArray, 3));
+function createAttributes(imagePositions) {
+  let attr = attributes;
+  return new Promise((resolve) => {
+    const count = 3;
+    let imageLen = imagePositions.length;
+    
+    for (var i = 0; imageLen > i; i++) {
+      attr.positions[i] = new Float32Array(MAX * count);
+      attr.endPositions[i] =  new Float32Array(MAX * count);
+      attr.alphas[i] = new Float32Array(MAX);
+      attr.colors[i] = new Float32Array(MAX);
+      const datas = imagePositions[i];
+      const datasLen = datas.length;
+      for (var n = 0; n < MAX; n++) {
+        var data = datas[parseInt(datasLen * Math.random())];
+        attr.positions[i][n * count] = data.x;
+        attr.positions[i][n * count + 1] = 0;
+        attr.positions[i][n * count + 2] = data.y;
+        
+        attr.endPositions[i][n * count] = range(0, 30);
+        attr.endPositions[i][n * count + 1] = range(0, 30);
+        attr.endPositions[i][n * count + 2] = range(0, 0);
+      }
     }
+    resolve(attr);
+  });
+}
+
+function  init(){
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+  camera.position.z = -30;
+  camera.position.y = 100;
+  camera.lookAt(new THREE.Vector3())
+
+  scene = new THREE.Scene();
+
+  particle = new THREE.Points(new CustomParticle(), new CustomMat());
+  scene.add(particle);
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x333333);
+  clock = new THREE.Clock();
+  document.body.appendChild(renderer.domElement);
+  document.addEventListener('mousemove', onDocumentMouseMove, false);
+  TweenMax.ticker.addEventListener("tick", loop);
+}
+
+
+class CustomParticle extends THREE.BufferGeometry {
+  constructor(){
+    super()
+    this.count = MAX;
+    var positionArray = new Float32Array(this.count * 3);
+    var endPositionArray = new Float32Array(this.count * 3);
+    var alphaArray = new Float32Array(this.count);
+    var timeArray = new Float32Array(this.count);
+    var colorArray = new Float32Array(this.count * 3);
+    const datas = imagePositions[imageIndex];
+    // @todo これを別のfunctionにと画像配列分
+    for(var ii = 0; ii < this.count; ii++){
+      var data = datas[parseInt(datas.length * Math.random())];
+      
+      positionArray[ii * 3 + 0] = data.x;
+      positionArray[ii * 3 + 1] = 0;
+      positionArray[ii * 3 + 2] = data.y;
+
+      endPositionArray[ii * 3 + 0] = range(0, 30);
+      endPositionArray[ii * 3 + 1] = range(0, 30);
+      endPositionArray[ii * 3 + 2] = range(0, 0);
+
+      timeArray[ii] = 3 * Math.random();
+
+      alphaArray[ii] = Math.random();
+
+      colorArray[ii * 3 + 0] = data.color.r;
+      colorArray[ii * 3 + 1] = data.color.g;
+      colorArray[ii * 3 + 2] = data.color.b;
+    }
+
+    this.addAttribute("position", new THREE.BufferAttribute(positionArray, 3));
+    this.addAttribute("aTarget", new THREE.BufferAttribute(endPositionArray, 3));
+    this.addAttribute("aTime", new THREE.BufferAttribute(timeArray, 1));
+    this.addAttribute("aAlpha", new THREE.BufferAttribute(alphaArray, 1));
+    this.addAttribute("aColor", new THREE.BufferAttribute(colorArray, 3));
+  }
 }
 
 class CustomMat extends THREE.ShaderMaterial {
-    constructor(){
-        var uniforms = {
-            uTime : {type : "f", value : 0}
-        };
-        super({
-            uniforms: uniforms,
-            vertexShader: document.getElementById("vertex").textContent,
-            fragmentShader: document.getElementById("fragment").textContent
-        });
+  constructor(){
+    var uniforms = {
+        uTime : {type : "f", value : 0}
+    };
+    super({
+        uniforms: uniforms,
+        vertexShader: document.getElementById("vertex").textContent,
+        fragmentShader: document.getElementById("fragment").textContent
+    });
 
-        this.transparent = true;
-        this.depthWrite = false;
-        this.blending = THREE.AdditiveBlending;
-    }
+    this.transparent = true;
+    this.depthWrite = false;
+    this.blending = THREE.AdditiveBlending;
+  }
 }
 
-function imageInit(){
-    image = new Image();
-    image.onload = onLoadimage;
-    image.crossOrigin = "Anonymous";
-    image.src = imgSrc;
-}
-
-/**
- * imagesInit
- * array image ver
- */
-async function imagesInit() {
-  var canvas = document.createElement("canvas");
-  imagePositions = await createImgPosition(canvas);
-  console.log(imagePositions);
-  init();
-}
 
 /**
  * createImgPosition
  * @param {*} canvas
  */
-function createImgPosition(canvas) {
+function createImagePositions(canvas) {
   return new Promise(async (resolve) => {
     let positions = [];
     var len = imgSrcArr.length;
@@ -144,6 +186,9 @@ function createImgPosition(canvas) {
   })
 }
 
+/**
+ * loadImage
+ */
 function loadImage(src) {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -154,66 +199,10 @@ function loadImage(src) {
   });
 }
 
-function  init(){
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-    camera.position.z = -30;
-    camera.position.y = 100;
-    camera.lookAt(new THREE.Vector3())
-
-    scene = new THREE.Scene();
-
-    particle = new THREE.Points(new CustomParticle(), new CustomMat());
-    scene.add(particle);
-
-    renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x333333);
-
-    clock = new THREE.Clock();
-    document.body.appendChild(renderer.domElement);
-
-    document.addEventListener('mousemove', onDocumentMouseMove, false);
-
-    // requestAnimationFrame
-    TweenMax.ticker.addEventListener("tick", loop);
+function range(min, max){
+  return min + (max - min) * Math.random();
 }
 
-function onLoadimage(){
-    var canvas = document.createElement("canvas");
-    canvas.width = image.width;
-    canvas.height = image.height;
-    var ctx = canvas.getContext('2d');
-
-    ctx.drawImage(image, 0, 0);
-
-    var data = ctx.getImageData(0, 0, image.width, image.height);
-    var imageData = data.data;
-    var scale = 10;
-
-    for(var yy = 0; yy < image.height; yy++){
-        for(var xx = 0; xx < image.width; xx++){
-            var num = 4 * (image.width * yy + xx ) + 3;
-            var alpha = imageData[num];
-            if(alpha !== 0){
-                var rRate = xx/image.width * .9
-                var gRate = yy/image.height * .2;
-                var color = new THREE.Color();
-                color.setRGB(rRate, gRate, 1);
-                // color.setRGB(colorRate, 0.1 * Math.random(), 1)
-                // color.setHSL(colorRate, 0.3 + 0.6 * Math.random(), 0.3 + 0.3 * Math.random())
-                var data = {
-                  x: (xx - image.width / 2 + 9 * Math.random()) / scale,
-                  y: (yy - image.height / 2) / scale,
-                  alpha: alpha / 255,
-                  color: color,
-                }
-                datas.push(data);
-            }
-
-        }
-    }
-    // init();
-}
 
 var theta = 0;
 var time = 0;
@@ -251,12 +240,8 @@ window.addEventListener("resize", function(ev){
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-window.addEventListener('keydown', function(ev){
-    switch(ev.which){
-        case 83:
-            console.log(particle.geometry.attributes)
-            var  positions = new Float32Array( MAX * 3 ); // 3 vertices per point
-            particle.geometry.setAttribute('position', new THREE.BufferAttribute( positions, 3 ) );
-            break;
-    }
+window.addEventListener('click', function(e){
+  console.log(particle.geometry.attributes)
+  var  positions = new Float32Array( MAX * 3 ); // 3 vertices per point
+  particle.geometry.setAttribute('position', new THREE.BufferAttribute( attributes.positions[1], 3 ) );
 });
