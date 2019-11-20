e42c920c953ea727cf5710a1172cd83c5297e726
jun <juuuuuun.dev@gmail.com>
2019-11-20 22:27:56 +0900

    add user agent context

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -1,13 +1,14 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useContext } from 'react';
 import { Canvas, useThree, useFrame } from 'react-three-fiber';
 import { srcData } from '../config/srcData';
+import AppContext from '../contexts/AppContext';
 
 export default function Particle() {
+  const value = useContext(AppContext);
+  console.log(value)
   return (
     <>
-    <div>
-      particle
-    </div>
+    {value}
     </>
   )
 }
