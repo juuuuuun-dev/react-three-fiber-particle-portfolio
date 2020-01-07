53f8f0ea6623b793ae43cf948b5129f9505b5565
jun <juuuuuun.dev@gmail.com>
2020-01-07 22:59:25 +0900

    add header blor

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,8 @@ const Header = () => {
   const appTitle = useStore(state => state.appTitle);
   const showContent = useStore(state => state.showContent);
   const navList = actions.getHasPathNavList();
-  const refs = React.useRef(navList.map((_, index) => index))
+  const headerRef = React.useRef();
+  const refs = React.useRef(navList.map((_, index) => index));
   const springFunc = (index) => {
     let color = "#ffffff";
     if (showContent && showContent === navList[index].path) {
@@ -19,18 +20,15 @@ const Header = () => {
     }
   }
   const [ navSprings, setNavSprings ] = useSprings(navList.length, index => (springFunc(index)));
-  // @todo ぼかし
+  // @todo header low height
   setNavSprings(index => (springFunc(index)));
   const headerSpring = useSpring({
-    color: showContent ? '#ccc' : "#fff",
-    backgroundColor: showContent ? "rgba(67,70,90, .8)" : "rgba(67,70,90, 0)",
-    // -ms-filter: blur(6px);
-    // filter: blur(6px);
+    backdropFilter:  showContent ? "blur(1px)" : 'blur(0px)',
+    backgroundColor: showContent ? "rgba(67,70,90, .7)" : "rgba(67,70,90, .0)",
   });
-  console.log(showContent);
   return (
     <>
-      <animated.div className="header" style={{ ...headerSpring }}>
+      <animated.div ref={headerRef} className="header" style={{ ...headerSpring }}>
         <h1 className="logo">{ appTitle }</h1>
         <ul className="nav">
         {navSprings.map((item, index) => {
