108af50160d76690ba0cc5de169a6a1499789ca3
junkata <junkata.dev@gmail.com>
2020-02-06 18:51:32 +0900

    edit color

--- src/pages/main.js
+++ src/pages/main.js
@@ -4,6 +4,7 @@ import MoveText from "../components/MoveText";
 import { Canvas } from "react-three-fiber";
 import useStore from "../contexts/store";
 import polyfill from '@juggle/resize-observer'
+import * as THREE from 'three'
 
 export function Main() {
   const canvasRef = React.useRef();
@@ -16,10 +17,13 @@ export function Main() {
           resize={{polyfill}}
           camera={{ position: [-5, 100, 50], near: 0.1, fov: 45, up: [0, 1, 0], zoom: 1, far: 10000 }}
           onCreated={ ({ gl }) => {
-            gl.setClearColor("#363b4e")
+            // gl.setClearColor("#363b4e")
+            gl.setClearColor(new THREE.Color("#363b4e"))
           }}
         >
-          <spotLight intensity={1.2} color="white" position={[20, 70, 0.0]} />
+                <pointLight distance={200} intensity={8} position={[40, 60, 0.0]} color="#363b4e" />
+
+          {/* <spotLight intensity={1.2} color="white" penumbra={1.5} position={[20, 70, 0.0]} /> */}
           {/* <ambientLight color="white" intensity={0.9} /> */}
           <React.Suspense fallback={ null }>
             <MoveText position={ [0, 4.2, 0] } />
