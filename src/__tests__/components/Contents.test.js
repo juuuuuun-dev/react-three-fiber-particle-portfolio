40c10f8cd73ae5fa028ff6f71cd39c712dcd22d8
jun <juuuuuun.dev@gmail.com>
2020-02-23 22:21:36 +0900

    edit tets

--- src/__tests__/components/Contents.test.js
+++ src/__tests__/components/Contents.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { unmountComponentAtNode } from 'react-dom';
-import { render, getByTestId, getByText } from '@testing-library/react'
-import { act } from '@testing-library/react-hooks'
+import { render, getByTestId, getByText, fireEvent } from '@testing-library/react'
 import Contents from '../../components/Contents';
-import navList from '../../config/navList';
-import en from '../../config/about/en';
+import enAbout from '../../config/about/en';
+import jaAbout from '../../config/about/ja';
 import i18n from '../../components/i18n';
+
+
 
 // jest.mock('react-spring', () => {
 //   return {
@@ -49,10 +49,39 @@ describe("Contents", () => {
     jest.restoreAllMocks()
   })
 
-  it('renders Contents', () => {
+  it('renders language default About Contents', () => {
     const { container } = render(<Contents />);
+    const contentAbout = getByTestId(container, 'content-ABOUT');
     const cover = getByTestId(container, 'cover');
     expect(cover).toBeDefined();
-    expect(cover.innerHTML).toBe(en.cover);
+    expect(cover.innerHTML).toBe(enAbout.cover);
+    testParagraph(contentAbout, enAbout.paragraph);
   });
+  it("language change JA About Contents", () => {
+    const { container } = render(<Contents />);
+    const contentAbout = getByTestId(container, 'content-ABOUT');
+    const cover = getByTestId(container, 'cover');
+    fireEvent.click(getByText(contentAbout, "JP"));
+    expect(cover.innerHTML).toBe(jaAbout.cover);
+    testParagraph(contentAbout, jaAbout.paragraph);
+
+  })
+  const testParagraph = (contentAbout, paragraph) => {
+    paragraph.map((value) => {
+      expect(getByText(contentAbout, value.title)).toBeDefined();
+      expect(contentAbout.innerHTML).toMatch(value.title);
+      if (value.description) {
+        expect(contentAbout.innerHTML).toMatch(value.description);
+      }
+      if (value.quotes) {
+        value.quotes.map((quote) => {
+          expect(contentAbout.innerHTML).toMatch(quote.title);
+          quote.items.map(item => {
+            expect(contentAbout.innerHTML).toMatch(item.body);
+            expect(contentAbout.innerHTML).toMatch(item.author);
+          })
+        })
+      }
+    })
+  }
 });
--- src/__tests__/components/Header.test.js
+++ src/__tests__/components/Header.test.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { render } from '@testing-library/react'
+
 import Header from '../../components/Header';
 import commonConfig from '../../config/common.json';
-import navList from '../../config/navList';
-// import { renderHook, act } from '@testing-library/react-hooks';
-
-let container = null;
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
 
 it('renders Header', () => {
-  act(() => {
-    render(<Header />, container);
-  });
+  const { container } = render(<Header />);
   expect(container.querySelector('.logo').textContent).toBe(
     commonConfig.appTitle
   );
