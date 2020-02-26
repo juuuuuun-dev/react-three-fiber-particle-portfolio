1f96cf90dd540ef7d86ce785f50cf5b4cc6b4be5
jun <juuuuuun.dev@gmail.com>
2020-02-26 22:41:13 +0900

    edit navListIndex

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,9 +2,7 @@ import React from 'react';
 import useStore from '../contexts/store';
 
 export default function () {
-  const navListIndex = useStore(state => state.navListIndex);
 
-  console.log("test");
   return (
     <>
       <mesh></mesh>
