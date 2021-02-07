85e77f8cd98ba3b859ec19ee8bab2a8e861785f5
jun <junkata.dev@gmail.com>
2021-02-07 11:00:02 +0900

    add iam user

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -43,9 +43,12 @@ module "s3_bucket_for_app_storage" {
 
 data "aws_iam_policy_document" "s3_static_website_policy" {
   statement {
-    sid     = "PublicReadAndPutGetObject"
-    effect  = "Allow"
-    actions = ["s3:GetObject"]
+    sid    = "PublicReadAndPutGetObject"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
     principals {
       type        = "*"
       identifiers = ["*"]
