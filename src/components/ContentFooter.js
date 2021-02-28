8d3eaad82854811296f5f8d3ee35128ebc64b866
jun <junkata.dev@gmail.com>
2021-02-28 21:10:25 +0900

    add ok cookie data

--- src/components/ContentFooter.js
+++ src/components/ContentFooter.js
@@ -6,12 +6,27 @@ const ContentFooter = () => {
   const domain = useStore(state => state.domain);
   const startYear = useStore(state => state.copyrightStartYear);
   const currnetYear = startYear !== nowYear ? `-${nowYear}` : '';
+  const lang = useStore(state => state.lang);
+  const defaultLang = useStore(state => state.defaultLang);
+  const langUri = lang === defaultLang ? '' : `/${lang}`
+  const actions = useStore(state => state.actions);
+  const cookiePath = '/cookies';
+
+  const handleLink = (e) => {
+    e.preventDefault();
+    actions.toggleContents(cookiePath);
+  }
+
   return (
     <>
       <div className='contents__footer'>
+        <p className="contents__footer-copyright">
         &copy; {startYear}
         {currnetYear} {domain}
+        </p>
+        <p className="cookie-message__body"><a href={`${langUri}${cookiePath}`} onClick={(e) => handleLink(e)}>Cookies policy</a></p>
       </div>
+      
     </>
   );
 };
--- src/components/CookieMessage.js
+++ src/components/CookieMessage.js
@@ -1,18 +1,34 @@
 import React from "react"
 import useStore from '../contexts/store';
+import { get, set } from 'local-storage';
 
 const CookieMessage = () => {
   const actions = useStore(state => state.actions);
-  const handleClick = (e) => {
+  const okCookieKey = "ok_cookie";
+  const cookiePath = '/cookies';
+  const okCookie = get(okCookieKey);
+  const [ok, setOK] = React.useState(okCookie);
+
+
+  const handleLink = (e) => {
     e.preventDefault();
-    actions.toggleContents('/cookies');
+    actions.toggleContents(cookiePath);
   }
+
+  const handleOK = () => {
+    set(okCookieKey, 1)
+    setOK(1)
+  }
+
   return (
     <>
+    {!ok &&
       <div className="cookie-message">
-        <p className="cookie-message__body">This website use cookies. <a href="/cookie" onClick={(e) => handleClick(e)}>Learn more</a>.</p>
-        <p className="cookie-message__btn">OK</p>
+        <p className="cookie-message__body">This website use cookies. <a href={`${cookiePath}`} onClick={(e) => handleLink(e)}>Learn more</a>.</p>
+        <p className="cookie-message__btn" onClick={handleOK}>OK</p>
       </div>
+    }
+      
     </>
   )
 }
